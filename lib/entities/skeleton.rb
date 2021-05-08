03109b58a62dc3ce0f3a2fc43ededa9d20b4c053
Kody Wilson <kodywilson@gmail.com>
2021-05-07 19:43:03 -0700

    closes #17 track enemy health and closes #18 hit and damage for players and closes #21 show current health with health bars

--- lib/entities/skeleton.rb
+++ lib/entities/skeleton.rb
@@ -5,7 +5,7 @@ class Skeleton
   SKEL_LOC = File.join(SPRITES, 'npc', 'skeleton')
 
   attr_reader :x, :y, :radius, :width, :height, :b_left, :b_right, :b_top, :b_low, :hit_timer
-  attr_accessor :attacking
+  attr_accessor :attacking, :current_health
 
   @@enemies_appeared ||= 0
 
@@ -16,6 +16,7 @@ class Skeleton
     @z = 2
     @radius = 16 # Trying this with my 32 pixel tiles
     @scale = 1
+    @color = Gosu::Color::RED
     @font = Gosu::Font.new(28)
     @images = load_images
     @frames = @images[:walk]
@@ -34,6 +35,8 @@ class Skeleton
     @direction = :left
     @velocity = 1
     @hit_timer = 0
+    @current_health = 10.00
+    @max_health = 10.00
   end
 
   def animation
@@ -43,9 +46,9 @@ class Skeleton
 
   def attack
     @attacking = true
-    @hit_timer = Gosu.milliseconds + 4000
+    @hit_timer = Gosu.milliseconds + 2000
     damage = 0
-    damage = rand(1..3) if rand(1..10) > 7
+    damage = rand(1..3) if rand(1..10) > 6
     return damage
   end
 
@@ -84,6 +87,9 @@ class Skeleton
   end
 
   def draw
+    # Health Bar
+    bar_x = @current_health >= 1 ? @current_health / @max_health : 0
+    Gosu::draw_line(@b_left,@b_top,@color,@b_left + @width * bar_x, @b_top,@color,2) unless @intro
     if @image_index < @frames.count
       @frames[@image_index].draw(@x, @y, 2, scale_x = @scale, scale_y = @scale)
       if @counter % 3 == 0
@@ -97,7 +103,7 @@ class Skeleton
       counter = 0
     end
     if DEBUG
-      @font.draw_text("Enemies appeared: #{@@enemies_appeared}",180,690,1,1,1,Gosu::Color::RED)
+      @font.draw_text("Enemies appeared: #{@@enemies_appeared}",180,690,1,1,1,@color)
     end
     #@frames[1].draw(@x, @y, 2, scale_x = @scale, scale_y = @scale)
   end
